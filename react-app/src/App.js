c8d00eab256377a964565a1f13cf89eb9988b147
fishcakefish <fishcakeow@gmail.com>
2023-09-07 06:31:07 -0700

    main page + nav css

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,6 +5,7 @@ import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
+import MainPage from "./components/MainPage";
 
 function App() {
   const dispatch = useDispatch();
@@ -23,6 +24,9 @@ function App() {
           </Route>
           <Route path="/signup">
             <SignupFormPage />
+          </Route>
+          <Route exact path="/">
+            <MainPage />
           </Route>
         </Switch>
       )}
